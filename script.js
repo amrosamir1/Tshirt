47e393f691fa06126e3ccd1d5b68bfba56991acc
Amr Samir <124004890+amrosamir1@users.noreply.github.com>
2023-04-29 21:59:30 +0300

    done

---
+++ script.js
@@ -0,0 +1,6 @@
+/* let incardcarts = document.getElementById('incardcarts')
+let card = document.getElementById('inCardsEdit')
+card.addEventListener("mouseover" , onHover())
+function onHover(){
+    incardcarts.style.opacity=1;
+} */
